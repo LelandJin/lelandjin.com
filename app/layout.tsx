cdc9201723f0349a24dbf8670d33f8afb29dd7e4
LelandJin <lnjsz@qq.com>
2023-11-13 04:11:45 +0000

    change name

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,23 +6,16 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "lelandjin.com",
+    template: "%s | lelandjin.com",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
-    title: "chronark.com",
+    title: "lelandjin.com",
     description:
       "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
-    images: [
-      {
-        url: "https://chronark.com/og.png",
-        width: 1920,
-        height: 1080,
-      },
-    ],
+    url: "https://lelandjin.com",
+    siteName: "lelandjin.com",
     locale: "en-US",
     type: "website",
   },
@@ -38,7 +31,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "Leland",
     card: "summary_large_image",
   },
   icons: {
